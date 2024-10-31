2035b131defbefad12c6c2306632773b99a18fc3
Cinnlight <cinnlight@gmail.com>
2024-10-30 23:25:07 -0600

    Comments out footer import for temporary testing

--- webfolio/src/main.jsx
+++ webfolio/src/main.jsx
@@ -1,9 +1,9 @@
 import React from "react";
-import ReactDom from "react-dom/client";
+import ReactDOM from "react-dom/client";
 import WebfolioRouter from "./router";
 
-createRoot(document.getElementById('root')).render(
-  <StrictMode>
+ReactDOM.createRoot(document.getElementById('root')).render(
+  <React.StrictMode>
     <WebfolioRouter />
-  </StrictMode>,
+  </React.StrictMode>,
 )
