2c5716f29440c319ac898ca57869d60066c5b1a1
Cinnlight <cinnlight@gmail.com>
2024-10-30 23:26:44 -0600

    Adds placeholder text and export to page for test rendering

---
+++ webfolio/src/pages/Projects.jsx
@@ -0,0 +1,12 @@
+
+
+function Projects() {
+    return (
+        <div className="container">
+            <h1>Projects</h1>
+            <p>Here are some of the projects that I have worked on.</p>
+        </div>
+    );
+}
+
+export default Projects;
