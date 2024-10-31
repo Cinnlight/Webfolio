6f271774691f8b21c34eba3b2ea2b8591c16fc51
Cinnlight <cinnlight@gmail.com>
2024-10-31 16:36:44 -0600

    Imports Navbar into Footer

---
+++ webfolio/src/components/Footer.jsx
@@ -0,0 +1,49 @@
+// import { Link } from 'react-router-dom';
+import { Button, Modal } from 'react-bootstrap';
+import "bootstrap/dist/css/bootstrap.min.css";
+import ContactForm from './ContactForm';
+import { useState } from 'react';
+import { FaGithub } from 'react-icons/fa';
+
+const Footer = () => {
+    const [show, setShow] = useState(false);
+
+    const handleClose = () => setShow(false);
+    const handleShow = () => setShow(true);
+
+    return (
+        <footer className="bg-dark text-white text-center py-4">
+            {/* Contact Me Button */}
+            <div className="container">
+                <p>© 2024 Cinnlight</p>
+                <span className="me-3">
+                    <Button variant="outline-light" onClick={handleShow}>
+                        Contact Me
+                    </Button>
+                </span>
+
+                {/* GitHub Link */}
+                <a
+                    href="https://github.com/cinnlight"
+                    target="_blank"
+                    rel="noopener noreferrer"
+                    className="github-icon"
+                >
+                    <FaGithub size={30} />
+                </a>
+
+                {/* Contact Modal */}
+                <Modal show={show} onHide={handleClose}>
+                    <Modal.Header closeButton>
+                        <Modal.Title>Contact Me</Modal.Title>
+                    </Modal.Header>
+                    <Modal.Body>
+                        <ContactForm handleClose={handleClose} />
+                    </Modal.Body>
+                </Modal>
+            </div>
+        </footer>
+    )
+}
+
+export default Footer
