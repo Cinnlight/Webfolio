659c41b97b93d63c23e5715023fa3af8c5881270
Cinnlight <cinnlight@gmail.com>
2024-10-30 23:26:31 -0600

    Adds placeholder text and export to page for test rendering

---
+++ webfolio/src/pages/Home.jsx
@@ -0,0 +1,12 @@
+
+
+function Home() {
+    return (
+        <div className="container">
+            <h1>Home</h1>
+            <p>Welcome to Webfolio, a simple web application that showcases a portfolio of web projects.</p>
+        </div>
+    );
+}
+
+export default Home;
