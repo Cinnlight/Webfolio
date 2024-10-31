be35a470bb1a6d9f5adb716eaa009ec219cbbf33
Cinnlight <cinnlight@gmail.com>
2024-10-30 23:26:09 -0600

    Adds placeholder text and export to page for test rendering

---
+++ webfolio/src/pages/About.jsx
@@ -0,0 +1,12 @@
+
+
+function About() {
+    return (
+        <div className="container">
+            <h1>About</h1>
+            <p>Webfolio is a simple web application that showcases a portfolio of web projects.</p>
+        </div>
+    );
+}
+
+export default About;
