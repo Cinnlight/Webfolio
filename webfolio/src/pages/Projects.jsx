f9f011354afba0dc088a5f967872d66e4c92b320
Cinnlight <cinnlight@gmail.com>
2024-11-01 16:10:48 -0600

    Utilizes ProjectCard to render 6 placeholder project cards for UI testing

--- webfolio/src/pages/Projects.jsx
+++ webfolio/src/pages/Projects.jsx
@@ -1,11 +1,63 @@
+import React from "react";
+import ProjectCard from "../components/ProjectCard";
+import placeholderIcon from "../assets/placeholder-icon.jpg";
+import { Container, Row, Col } from "react-bootstrap";
 
 
 function Projects() {
     return (
-        <div className="container">
-            <h1>Projects</h1>
-            <p>Here are some of the projects that I have worked on.</p>
-        </div>
+        <Container className="portfolio my-5">
+            <Row className="g-4">
+                <Col xs={12} sm={6} md={4}>
+                    <ProjectCard
+                        title="Project 1"
+                        description="This is a project description."
+                        repoLink="https://github.com/Cinnlight/employee-tracker"
+                        image={placeholderIcon}
+                    />
+                </Col>
+                <Col xs={12} sm={6} md={4}>
+                    <ProjectCard
+                        title="Project 2"
+                        description="This is a project description."
+                        repoLink="https://github.com/Cinnlight/employee-tracker"
+                        image={placeholderIcon}
+                    />
+                </Col>
+                <Col xs={12} sm={6} md={4}>            
+                    <ProjectCard
+                        title="Project 3"
+                        description="This is a project description."
+                        repoLink="https://github.com/Cinnlight/employee-tracker"
+                        image={placeholderIcon}
+                    />
+                </Col>
+                <Col xs={12} sm={6} md={4}>
+                    <ProjectCard
+                        title="Project 4"
+                        description="This is a project description."
+                        repoLink="https://github.com/Cinnlight/employee-tracker"
+                        image={placeholderIcon}
+                    />
+                </Col>
+                <Col xs={12} sm={6} md={4}>
+                    <ProjectCard
+                        title="Project 5"
+                        description="This is a project description."
+                        repoLink="https://github.com/Cinnlight/employee-tracker"
+                        image={placeholderIcon}
+                    />
+                </Col>
+                <Col xs={12} sm={6} md={4}>
+                    <ProjectCard
+                        title="Project 6"
+                        description="This is a project description."
+                        repoLink="https://github.com/Cinnlight/employee-tracker"
+                        image={placeholderIcon}
+                    />
+                </Col>
+            </Row>
+        </Container>
     );
 }
 
