65bc7b0f0554fa0be6c6104c63d4b94a85c90b09
Cinnlight <cinnlight@gmail.com>
2024-11-05 16:42:03 -0700

    Removes erroneous import

--- webfolio/src/components/ProjectCard.jsx
+++ webfolio/src/components/ProjectCard.jsx
@@ -1,4 +1,4 @@
-import React, { useState, useEffect } from 'react';
+import React, { useState } from 'react';
 import { Card, Modal, Carousel, Button } from 'react-bootstrap';
 import { FaGithub } from 'react-icons/fa';
 import '../styles/ProjectCard.css';
