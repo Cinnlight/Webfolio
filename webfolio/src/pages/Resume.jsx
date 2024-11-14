8660e167ada2357c6e5f9ccd498dcda4a87c54df
Cinnlight <cinnlight@gmail.com>
2024-11-14 11:16:29 -0700

    Add resume page

---
+++ webfolio/src/pages/Resume.jsx
@@ -0,0 +1,12 @@
+
+
+function Resume() {
+    return (
+        <div className="dark-overlay mx-auto w-75 mt-5 mb-5">
+            <h1>Resume</h1>
+            <p>Webfolio is a simple web application that showcases a portfolio of web projects.</p>
+        </div>
+    );
+}
+
+export default Resume;
