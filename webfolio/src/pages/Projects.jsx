bdec79af02588983252d4473c758e4ee03abfc09
Cinnlight <cinnlight@gmail.com>
2025-02-25 16:58:20 -0700

    Adds imports and images into two additional project cards

--- webfolio/src/pages/Projects.jsx
+++ webfolio/src/pages/Projects.jsx
@@ -3,12 +3,25 @@ import ProjectCard from "../components/ProjectCard";
 import { Container, Row, Col } from "react-bootstrap";
 
 // Asset Imports //
-// ArcadeHype
 import placeholderIcon from "../assets/placeholder-icon.jpg";
+
+// ArcadeHype
 import ArcadeHypeDarkMode from "../assets/project-assets/ArcadeHype-DarkMode.png"
 import ArcadeHypeLightMode from "../assets/project-assets/ArcadeHype-LightMode.png"
 import ArcadeHypeMobile from "../assets/project-assets/ArcadeHype-Mobile.png"
 import ArcadeHypeGif from "../assets/project-assets/ArcadeHypeGif.gif"
+
+// FantasyFighter
+import FantasyFighterCombatGif from "../assets/project-assets/FantasyFighter-combatGif.gif"
+import FantasyFighterCombat from "../assets/project-assets/FantasyFighter-combat.png"
+import FantasyFighterLevels from "../assets/project-assets/FantasyFighter-levels.png"
+import FantasyFighterLogin from "../assets/project-assets/FantasyFighter-login"
+
+// HyruleCompendium
+import HyruleCompendiumHome from "../assets/project-assets/HyruleCompendium-home"
+import HyruleCompendiumLogin from "../assets/project-assets/HyruleCompendium-login"
+import HyruleCompendiumPage from "../assets/project-assets/HyruleCompendium-page"
+import HyruleCompendiumVerify from "../assets/project-assets/HyruleCompendium-verify"
 
 
 function Projects() {
@@ -29,20 +42,20 @@ function Projects() {
                 </Col>
                 <Col xs={12} sm={6} md={4}>
                     <ProjectCard
-                        title="Project 2"
-                        description="This is a project description."
-                        repoLink="https://github.com/Cinnlight/employee-tracker"
-                        image={placeholderIcon}
-                        images={[placeholderIcon]}
+                        title="Fantasy Fighter"
+                        description="A classic turn-based RPG for the web."
+                        repoLink="https://github.com/MagicInUse/Fantasy-Fighter"
+                        image={FantasyFighterCombatGif}
+                        images={[FantasyFighterLogin, FantasyFighterLevels, FantasyFighterCombat, FantasyFighterCombatGif]}
                     />
                 </Col>
                 <Col xs={12} sm={6} md={4}>            
                     <ProjectCard
-                        title="Project 3"
+                        title="Hyrule Compendium"
                         description="This is a project description."
-                        repoLink="https://github.com/Cinnlight/employee-tracker"
-                        image={placeholderIcon}
-                        images={[placeholderIcon]}
+                        repoLink="https://github.com/Cinnlight/Hyrule-Compendium"
+                        image={HyruleCompendiumHome}
+                        images={[HyruleCompendiumPage, HyruleCompendiumLogin, HyruleCompendiumVerify]}
                     />
                 </Col>
                 <Col xs={12} sm={6} md={4}>
