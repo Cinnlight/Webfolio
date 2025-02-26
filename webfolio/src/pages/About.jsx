8813538eb0d9009e47bdff1d5d0c4eadbcfc3721
Cinnlight <cinnlight@gmail.com>
2025-02-26 09:26:26 -0700

    Updates about page to include a little about me.

--- webfolio/src/pages/About.jsx
+++ webfolio/src/pages/About.jsx
@@ -2,7 +2,13 @@ function About() {
     return (
         <div className="dark-overlay mx-auto w-75 mt-5 mb-5">
             <h1>About</h1>
-            <p>Webfolio is a simple web application that showcases a portfolio of web projects.</p>
+            <p>
+                Welcome to my portfolio! I enjoy many aspects of development, but databases and server-sided programming are my favorite. I spent 6 months studying with 
+                <a href="https://www.theodinproject.com/" target="_blank" rel="noopener noreferrer"> The Oden Project</a>, and another 6 months with a university hosted bootcamp for full-stack development.
+            </p>
+            <p>
+                I've been an avid gamer and computer enthusiast since I was a child. I've worked in the medical field for 10 years as a phlebotomist, assisting patients and doctors alike. I'm now transitioning into programming, fulfilling a life-long dream of following a career in technology.
+            </p>
         </div>
     );
 }
